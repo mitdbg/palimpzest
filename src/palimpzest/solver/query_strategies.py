3770e3f646463dc1f59e19c8e7a2457fd7fdadc7
mdr223 <mdr223@cornell.edu>
2024-03-29 00:23:36 -0400

    forgot to add changes to query_strategies as part of resolving MC w/Mike's changes

--- src/palimpzest/solver/query_strategies.py
+++ src/palimpzest/solver/query_strategies.py
@@ -10,7 +10,7 @@ import base64
 import json
 
 
-def _construct_bonded_query_prompt(td: TaskDescriptor, doc_type: str, generate_field_names: List[str]) -> str:
+def _construct_query_prompt(td: TaskDescriptor, doc_type: str, generate_field_names: List[str]) -> str:
     """
     This function constructs the prompt for a bonded query.
     """
@@ -30,30 +30,39 @@ def _construct_bonded_query_prompt(td: TaskDescriptor, doc_type: str, generate_f
     optionalInputDesc = "" if td.inputSchema.__doc__ is None else f"Here is a description of the input object: {td.inputSchema.__doc__}."
     optionalOutputDesc = "" if td.outputSchema.__doc__ is None else f"Here is a description of the output object: {td.outputSchema.__doc__}."
 
+    # construct sentence fragments which depend on cardinality of conversion ("oneToOne" or "oneToMany")
+    targetOutputDescriptor = "an output JSON object that describes an object of type"
+    outputSingleOrPlural = "the output object"
+    appendixInstruction = "Be sure to emit a JSON object only"
+    if td.cardinality == "oneToMany":
+        targetOutputDescriptor = "an output array of zero or more JSON objects that describe objects of type"
+        outputSingleOrPlural = "the output objects"
+        appendixInstruction = "Be sure to emit a JSON object only. The root-level JSON object should have a single field, called 'items' that is a list of the output objects. Every output object in this list should be a dictionary with the output fields described above. You must decide the correct number of output objects."
+
     # construct promptQuestion
     promptQuestion = None
     if td.prompt_strategy != PromptStrategy.IMAGE_TO_TEXT:
-        promptQuestion = f"""I would like you to create an output JSON object that describes an object of type {doc_type}. 
+        promptQuestion = f"""I would like you to create {targetOutputDescriptor} {doc_type}. 
         You will use the information in an input JSON object that I will provide. The input object has type {td.inputSchema.className()}.
-        All of the fields in the output object can be derived using information from the input object.
+        All of the fields in {outputSingleOrPlural} can be derived using information from the input object.
         {optionalInputDesc}
         {optionalOutputDesc}
         Here is every input field name and a description: 
         {multilineInputFieldDescription}
         Here is every output field name and a description:
         {multilineOutputFieldDescription}.
-        Be sure to emit a JSON object only.
+        {appendixInstruction}
         """ + "" if td.conversionDesc is None else f" Keep in mind that this process is described by this text: {td.conversionDesc}."                
-    
+
     else:
-        promptQuestion = f"""You are an image analysis bot. Analyze the supplied image and create an output JSON object that describes an object of type {doc_type}. 
+        promptQuestion = f"""You are an image analysis bot. Analyze the supplied image and create {targetOutputDescriptor} {doc_type}.
         You will use the information in the image that I will provide. The input image has type {td.inputSchema.className()}.
-        All of the fields in the output object can be derived using information from the input image.
+        All of the fields in {outputSingleOrPlural} can be derived using information from the input image.
         {optionalInputDesc}
         {optionalOutputDesc}
         Here is every output field name and a description:
         {multilineOutputFieldDescription}.
-        Be sure to emit a JSON object only.
+        {appendixInstruction}
         """ + "" if td.conversionDesc is None else f" Keep in mind that this process is described by this text: {td.conversionDesc}." 
 
     return promptQuestion
@@ -87,18 +96,42 @@ def _get_JSON_from_answer(answer: str) -> Dict[str, Any]:
     return json.loads(answer)
 
 
-def runBondedQuery(candidate: DataRecord, td: TaskDescriptor, verbose: bool=False) -> Tuple[DataRecord, Stats, str]:
-    # initialize output data record
+def _create_data_record_from_json(jsonObj: Any, td: TaskDescriptor, candidate: DataRecord) -> DataRecord:
+    # initialize data record
     dr = DataRecord(td.outputSchema)
     dr.parent_uuid = candidate.uuid
 
-    # copy fields from the candidate (input) record if they already exist
-    # and construct list of fields in outputSchema which will need to be generated
-    generate_field_names = []
+    # copy fields from the candidate (input) record if they already exist,
+    # otherwise parse them from the generated jsonObj
     for field_name in td.outputSchema.fieldNames():
         if field_name in td.inputSchema.fieldNames():
             setattr(dr, field_name, getattr(candidate, field_name))
         else:
+            # parse the json object and set the DataRecord's fields with their generated values 
+            setattr(dr, field_name, jsonObj[field_name])
+
+    return dr
+
+
+def runBondedQuery(candidate: DataRecord, td: TaskDescriptor, verbose: bool=False) -> Tuple[List[DataRecord], Stats, str]:
+    """
+    Run a bonded query, in which all new fields in the outputSchema are generated simultaneously
+    in a single LLM call. This is in contrast to a conventional query, in which each output field
+    is generated using its own LLM call.
+
+    At the moment, tasks with cardinality == "oneToMany" can only be executed using bonded queries.
+
+    This is not a theoretical limitation of conventional queries, but there are some practical
+    difficulties with guaranteeing that each field has the same number of outputs generated
+    in each separate LLM invocation.
+    """
+    # initialize list of output data records
+    drs = []
+
+    # construct list of fields in outputSchema which will need to be generated
+    generate_field_names = []
+    for field_name in td.outputSchema.fieldNames():
+        if field_name not in td.inputSchema.fieldNames():
             generate_field_names.append(field_name)
 
     # fetch input information
@@ -107,7 +140,7 @@ def runBondedQuery(candidate: DataRecord, td: TaskDescriptor, verbose: bool=Fals
     doc_type = td.outputSchema.className()
 
     # construct prompt question
-    promptQuestion = _construct_bonded_query_prompt(td, doc_type, generate_field_names)
+    promptQuestion = _construct_query_prompt(td, doc_type, generate_field_names)
 
     # generate LLM response and capture statistics
     answer, query_stats = None, None
@@ -144,17 +177,27 @@ def runBondedQuery(candidate: DataRecord, td: TaskDescriptor, verbose: bool=Fals
         # parse JSON object from the answer
         jsonObj = _get_JSON_from_answer(answer)
 
-        # set the DataRecord's fields with their generated values 
-        for field_name in generate_field_names:
-            setattr(dr, field_name, jsonObj[field_name])
+        # parse JSON output and construct data records
+        if td.cardinality == "oneToMany":
+            for elt in jsonObj["items"]:
+                dr = _create_data_record_from_json(elt, td, candidate)
+                drs.append(dr)
+        else:
+            dr = _create_data_record_from_json(elt, td, candidate)
+            drs = [dr]
 
     except Exception as e:
         return None, query_stats, str(e)
 
-    return dr, query_stats, None
+    return drs, query_stats, None
 
 
 def runConventionalQuery(candidate: DataRecord, td: TaskDescriptor, verbose: bool=False) -> Tuple[DataRecord, Stats]:
+    """
+    Run a conventional query, in which each output field is generated using its own LLM call.
+
+    At the moment, conventional queries cannot execute tasks with cardinality == "oneToMany".
+    """
     # initialize output data record
     dr = DataRecord(td.outputSchema)
     dr.parent_uuid = candidate.uuid
@@ -176,8 +219,8 @@ def runConventionalQuery(candidate: DataRecord, td: TaskDescriptor, verbose: boo
     # iterate over fields and generate their values using an LLM
     query_stats = {}
     for field_name in generate_field_names:
-        f = getattr(td.outputSchema, field_name)
-        promptQuestion = f"What is the {field_name} of the {doc_type}? ({f.desc})" + "" if td.conversionDesc is None else f" Keep in mind that this output is described by this text: {td.conversionDesc}."
+        # construct prompt question
+        promptQuestion = _construct_query_prompt(td, doc_type, [field_name])
         try:
             field_stats = None
             if td.prompt_strategy == PromptStrategy.DSPY_COT_QA:
