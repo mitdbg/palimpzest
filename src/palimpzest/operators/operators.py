340fce1d50a5eaac530258efb3ed655aaa80dea1
mdr223 <mdr223@cornell.edu>
2024-04-30 13:32:42 -0400

    add missing arg

--- src/palimpzest/operators/operators.py
+++ src/palimpzest/operators/operators.py
@@ -248,7 +248,7 @@ class LogicalOperator:
             for subTreePhysicalPlan in subTreePhysicalPlans:
                 for model in models:
                     # if model selection is disallowed; skip any plans which would use a different model in this operator
-                    subtree_models = [m for m in get_models_from_subtree() if m is not None]
+                    subtree_models = [m for m in get_models_from_subtree(subTreePhysicalPlan) if m is not None]
                     if not allow_model_selection and len(subtree_models) > 0 and subtree_models[0] != model:
                         continue
 
