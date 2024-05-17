d022b7257229afaf5bc6e67060caa3f256240f90
mdr223 <mdr223@cornell.edu>
2024-05-17 00:10:21 -0400

    adding plot w/marker and color

--- tests/create-final-plots.py
+++ tests/create-final-plots.py
@@ -38,7 +38,6 @@ def get_color(opt, workload, result_dict):
     elif workload == "real-estate":
         # give color to logical re-orderings on real-estate
         if result_dict['plan_info']['models'][1] == "gpt-4-vision-preview":
-            print(result_dict)
             color = opt_to_color[opt]
     
     return color
@@ -73,6 +72,7 @@ def plot_runtime_cost_vs_quality(results):
     fig_text, axs_text = plt.subplots(nrows=2, ncols=3, figsize=(15,6))
     fig_clean, axs_clean = plt.subplots(nrows=2, ncols=3, figsize=(15,6))
     fig_clean_bw, axs_clean_bw = plt.subplots(nrows=2, ncols=3, figsize=(15,6))
+    fig_clean_mc, axs_clean_mc = plt.subplots(nrows=2, ncols=3, figsize=(15,6))
 
     workload_to_col = {"enron": 0, "real-estate": 1, "biofabric": 2}
 
@@ -93,7 +93,8 @@ def plot_runtime_cost_vs_quality(results):
 
                 # set label and color
                 color = get_color(opt, workload, result_dict)
-                marker = '+' if color == "black" else None
+                marker = 'D' if color == "black" else None
+                mcolor = "black" if color == "black" else "#b33dc6"
 
                 # plot runtime vs. f1_score and cost vs. f1_score
                 axs_text[0][col].scatter(f1_score, runtime, alpha=0.6, color=color)
@@ -102,6 +103,8 @@ def plot_runtime_cost_vs_quality(results):
                 axs_clean[1][col].scatter(f1_score, cost, alpha=0.6, color=color)
                 axs_clean_bw[0][col].scatter(f1_score, runtime, alpha=0.6, color="black", marker=marker)
                 axs_clean_bw[1][col].scatter(f1_score, cost, alpha=0.6, color="black", marker=marker)
+                axs_clean_mc[0][col].scatter(f1_score, runtime, alpha=0.6, color=mcolor, marker=marker)
+                axs_clean_mc[1][col].scatter(f1_score, cost, alpha=0.6, color=mcolor, marker=marker)
 
                 # add annotations
                 axs_text[0][col].annotate(text, (f1_score, runtime))
@@ -134,6 +137,8 @@ def plot_runtime_cost_vs_quality(results):
         axs_clean[1][col].plot(pareto_cost_xs, pareto_cost_ys, color="#ef9b20", linestyle='--')
         axs_clean_bw[0][col].plot(pareto_runtime_xs, pareto_runtime_ys, color="#ef9b20", linestyle='--')
         axs_clean_bw[1][col].plot(pareto_cost_xs, pareto_cost_ys, color="#ef9b20", linestyle='--')
+        axs_clean_mc[0][col].plot(pareto_runtime_xs, pareto_runtime_ys, color="#ef9b20", linestyle='--')
+        axs_clean_mc[1][col].plot(pareto_cost_xs, pareto_cost_ys, color="#ef9b20", linestyle='--')
 
         # set x,y-lim for each workload
         left, right = -0.05, 1.05
@@ -158,6 +163,11 @@ def plot_runtime_cost_vs_quality(results):
         axs_clean_bw[1][col].set_xlim(left, right)
         if workload != "biofabric":
             axs_clean_bw[1][col].set_ylim(ymin=0)
+        axs_clean_mc[0][col].set_xlim(left, right)
+        axs_clean_mc[0][col].set_ylim(ymin=0)
+        axs_clean_mc[1][col].set_xlim(left, right)
+        if workload != "biofabric":
+            axs_clean_mc[1][col].set_ylim(ymin=0)
 
         # turn on grid lines
         axs_text[0][col].grid(True, alpha=0.4)
@@ -166,12 +176,15 @@ def plot_runtime_cost_vs_quality(results):
         axs_clean[1][col].grid(True, alpha=0.4)
         axs_clean_bw[0][col].grid(True, alpha=0.4)
         axs_clean_bw[1][col].grid(True, alpha=0.4)
+        axs_clean_mc[0][col].grid(True, alpha=0.4)
+        axs_clean_mc[1][col].grid(True, alpha=0.4)
 
     # remove ticks from first row
     for idx in range(3):
         axs_text[0][idx].set_xticklabels([])
         axs_clean[0][idx].set_xticklabels([])
         axs_clean_bw[0][idx].set_xticklabels([])
+        axs_clean_mc[0][idx].set_xticklabels([])
 
     # savefigs
     workload_to_title = {
@@ -183,6 +196,8 @@ def plot_runtime_cost_vs_quality(results):
         idx = 0 if workload == "enron" else (1 if workload == "real-estate" else 2)
         axs_text[0][idx].set_title(f"{title}", fontsize=12)
         axs_clean[0][idx].set_title(f"{title}", fontsize=12)
+        axs_clean_bw[0][idx].set_title(f"{title}", fontsize=12)
+        axs_clean_mc[0][idx].set_title(f"{title}", fontsize=12)
 
     axs_text[0][0].set_ylabel("Runtime (seconds)", fontsize=12)
     axs_text[1][0].set_ylabel("Cost (USD)", fontsize=12)
@@ -199,9 +214,15 @@ def plot_runtime_cost_vs_quality(results):
     for idx in range(3):
         axs_clean_bw[1][idx].set_xlabel("F1 Score", fontsize=12)
 
+    axs_clean_mc[0][0].set_ylabel("Runtime (seconds)", fontsize=12)
+    axs_clean_mc[1][0].set_ylabel("Cost (USD)", fontsize=12)
+    for idx in range(3):
+        axs_clean_mc[1][idx].set_xlabel("F1 Score", fontsize=12)
+
     fig_text.savefig(f"final-eval-results/plots/all-text.png", dpi=500, bbox_inches="tight")
     fig_clean.savefig(f"final-eval-results/plots/all-clean.png", dpi=500, bbox_inches="tight")
     fig_clean_bw.savefig(f"final-eval-results/plots/all-clean-bw.png", dpi=500, bbox_inches="tight")
+    fig_clean_mc.savefig(f"final-eval-results/plots/all-clean-mc.png", dpi=500, bbox_inches="tight")
 
 
 def plot_reopt(results, policy):
