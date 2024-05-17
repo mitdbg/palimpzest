6507127017ee31f4b7b3843b8b6b9a81739fc91a
mdr223 <mdr223@cornell.edu>
2024-05-17 08:37:25 -0400

    trying second plot

--- tests/create-final-plots.py
+++ tests/create-final-plots.py
@@ -450,14 +450,16 @@ if __name__ == "__main__":
             "min-cost-at-fixed-quality": {
                 # "enron": ("token-reduction", 0), # ("model", 10),
                 "enron": ("token-reduction", 1),
-                "real-estate": ("codegen", 0),
+                # "real-estate": ("codegen", 0),
+                "real-estate": ("token-reduction", 9),
                 "biofabric": ("token-reduction", 8),
             },
             ### min runtime
             "min-runtime-at-fixed-quality": {
                 # "enron": ("token-reduction", 0), # ("model", 10),
                 "enron": ("token-reduction", 1),
-                "real-estate": ("codegen", 0),
+                # "real-estate": ("codegen", 0),
+                "real-estate": ("token-reduction", 9),
                 "biofabric": ("model", 0),
             },
         }
