d82c09910ae0325d18b2beb36b92e08b0eb0dad6
vitaglianog <vitaglianog@gmail.com>
2024-08-30 17:26:26 -0400

    Updated register script

--- testdata/register-sources.sh
+++ testdata/register-sources.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
-pz reg --path testdata/real-estate-eval-20 --name real-estate-eval-20
-pz reg --path testdata/biofabric-tiny-filtered --name biofabric-tiny-filtered
-pz reg --path testdata/real-estate-eval-tiny --name real-estate-eval-tiny
-pz reg --path testdata/groundtruth --name groundtruth
-pz reg --path testdata/askem --name askem
-pz reg --path testdata/enron-eval --name enron-eval
-pz reg --path testdata/pdfs-tiny --name pdfs-tiny
-pz reg --path testdata/enron-tiny --name enron-tiny
-pz reg --path testdata/enron-small --name enron-small
-pz reg --path testdata/bdf-usecase3-references-pdffull --name bdf-usecase3-references-pdffull
-pz reg --path testdata/bdf-usecase3-tiny --name bdf-usecase3-tiny
-pz reg --path testdata/real-estate-eval-15 --name real-estate-eval-15
-pz reg --path testdata/skilling-raptor-emails.txt --name skilling-raptor-emails.txt
-pz reg --path testdata/vldbdownload --name vldbdownload
-pz reg --path testdata/images-tiny --name images-tiny
-pz reg --path testdata/bdf-usecase3-references --name bdf-usecase3-references
-pz reg --path testdata/equation-tiny --name equation-tiny
-pz reg --path testdata/biofabric-urls --name biofabric-urls
-pz reg --path testdata/enron-eval-tiny --name enron-eval-tiny
-pz reg --path testdata/biofabric-pdf --name biofabric-pdf
-pz reg --path testdata/biofabric-tiny --name biofabric-tiny
-pz reg --path testdata/bdf-usecase3-references-pdf --name bdf-usecase3-references-pdf
-pz reg --path testdata/target_matching.csv --name target_matching.csv
-pz reg --path testdata/real-estate-eval --name real-estate-eval
-pz reg --path testdata/biofabric-html --name biofabric-html
-pz reg --path testdata/bdf-usecase3-pdf --name bdf-usecase3-pdf
-pz reg --path testdata/real-estate-eval-5 --name real-estate-eval-5
-pz reg --path testdata/real-estate-eval-25 --name real-estate-eval-25
-pz reg --path testdata/real-estate-eval-30 --name real-estate-eval-30
-pz reg --path testdata/biofabric-matching --name biofabric-matching
-pz reg --path testdata/biofabric-medium --name biofabric-medium
-pz reg --path testdata/askem-tiny --name askem-tiny
-pz reg --path testdata/real-estate-eval-10 --name real-estate-eval-10
+# for loop to register all the sources in the testdata directory
+for file in testdata/*; do
+  if [ -d "$file" ]; then
+    pz reg --path $file --name $(basename $file)
+  fi
+done
