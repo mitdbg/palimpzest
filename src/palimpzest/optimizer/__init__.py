7d1f42bde7347a83495cd9ca76e9f259ca1c8c7b
Rami Pellumbi <ramipellumbi@gmail.com>
2024-10-26 21:42:16 -0400

    Prevent __init__ imports from operators and optimizer

--- src/palimpzest/optimizer/__init__.py
+++ src/palimpzest/optimizer/__init__.py
@@ -1,50 +1,84 @@
 from palimpzest.optimizer.rules import (
-    AggregateRule,
-    BasicSubstitutionRule,
-    CodeSynthesisConvertRule,
-    CodeSynthesisConvertSingleRule,
-    ImplementationRule,
-    LLMConvertBondedRule,
-    LLMConvertConventionalRule,
-    LLMConvertRule,
-    LLMFilterRule,
-    NonLLMConvertRule,
-    NonLLMFilterRule,
-    PushDownFilter,
-    Rule,
-    TokenReducedConvertBondedRule,
-    TokenReducedConvertConventionalRule,
-    TokenReducedConvertRule,
-    TransformationRule,
+    AggregateRule as _AggregateRule,
+)
+from palimpzest.optimizer.rules import (
+    BasicSubstitutionRule as _BasicSubstitutionRule,
+)
+from palimpzest.optimizer.rules import (
+    CodeSynthesisConvertRule as _CodeSynthesisConvertRule,
+)
+from palimpzest.optimizer.rules import (
+    CodeSynthesisConvertSingleRule as _CodeSynthesisConvertSingleRule,
+)
+from palimpzest.optimizer.rules import (
+    ImplementationRule as _ImplementationRule,
+)
+from palimpzest.optimizer.rules import (
+    LLMConvertBondedRule as _LLMConvertBondedRule,
+)
+from palimpzest.optimizer.rules import (
+    LLMConvertConventionalRule as _LLMConvertConventionalRule,
+)
+from palimpzest.optimizer.rules import (
+    LLMConvertRule as _LLMConvertRule,
+)
+from palimpzest.optimizer.rules import (
+    LLMFilterRule as _LLMFilterRule,
+)
+from palimpzest.optimizer.rules import (
+    NonLLMConvertRule as _NonLLMConvertRule,
+)
+from palimpzest.optimizer.rules import (
+    NonLLMFilterRule as _NonLLMFilterRule,
+)
+from palimpzest.optimizer.rules import (
+    PushDownFilter as _PushDownFilter,
+)
+from palimpzest.optimizer.rules import (
+    Rule as _Rule,
+)
+from palimpzest.optimizer.rules import (
+    TokenReducedConvertBondedRule as _TokenReducedConvertBondedRule,
+)
+from palimpzest.optimizer.rules import (
+    TokenReducedConvertConventionalRule as _TokenReducedConvertConventionalRule,
+)
+from palimpzest.optimizer.rules import (
+    TokenReducedConvertRule as _TokenReducedConvertRule,
+)
+from palimpzest.optimizer.rules import (
+    TransformationRule as _TransformationRule,
 )
 
 ALL_RULES = [
-    AggregateRule,
-    BasicSubstitutionRule,
-    CodeSynthesisConvertRule,
-    CodeSynthesisConvertSingleRule,
-    ImplementationRule,
-    LLMConvertBondedRule,
-    LLMConvertConventionalRule,
-    LLMConvertRule,
-    LLMFilterRule,
-    NonLLMConvertRule,
-    NonLLMFilterRule,
-    PushDownFilter,
-    Rule,
-    TokenReducedConvertBondedRule,
-    TokenReducedConvertConventionalRule,
-    TokenReducedConvertRule,
-    TransformationRule,
+    _AggregateRule,
+    _BasicSubstitutionRule,
+    _CodeSynthesisConvertRule,
+    _CodeSynthesisConvertSingleRule,
+    _ImplementationRule,
+    _LLMConvertBondedRule,
+    _LLMConvertConventionalRule,
+    _LLMConvertRule,
+    _LLMFilterRule,
+    _NonLLMConvertRule,
+    _NonLLMFilterRule,
+    _PushDownFilter,
+    _Rule,
+    _TokenReducedConvertBondedRule,
+    _TokenReducedConvertConventionalRule,
+    _TokenReducedConvertRule,
+    _TransformationRule,
 ]
+
 IMPLEMENTATION_RULES = [
     rule
     for rule in ALL_RULES
-    if issubclass(rule, ImplementationRule)
-    and rule not in [ImplementationRule, LLMConvertRule, TokenReducedConvertRule, CodeSynthesisConvertRule]
+    if issubclass(rule, _ImplementationRule)
+    and rule not in [_ImplementationRule, _LLMConvertRule, _TokenReducedConvertRule, _CodeSynthesisConvertRule]
 ]
+
 TRANSFORMATION_RULES = [
-    rule for rule in ALL_RULES if issubclass(rule, TransformationRule) and rule not in [TransformationRule]
+    rule for rule in ALL_RULES if issubclass(rule, _TransformationRule) and rule not in [_TransformationRule]
 ]
 
 __all__ = [
