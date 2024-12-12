1144c391ce61e8ff4ae76f7bff2d1cdfe1e34332
vitaglianog <vitaglianog@gmail.com>
2024-12-12 13:53:59 -0500

    Added test to run enron with dynamically parsed schema

--- tests/pytest/test_dynamicschema.py
+++ tests/pytest/test_dynamicschema.py
@@ -1,25 +1,74 @@
 """This testing class tests whether we can run a workload by defining a schema dynamically."""
 
 import palimpzest as pz
+from palimpzest.constants import Model
+
+data_path = "tests/pytest/data/"
 
 
 def test_dynamicschema_jsonld():
-
-    email_schema = pz.SchemaBuilder.from_file("data/email_schema.jsonld", schema_type=pz.TextFile)
-    assert email_schema is not None
-    assert issubclass(email_schema, pz.TextFile)
-
-def test_dynamicschema_json():
-    email_schema = pz.SchemaBuilder.from_file("data/email_schema.json")
-    assert email_schema is not None
-    assert issubclass(email_schema, pz.TextFile)
-
-def test_dynamicschema_yml():
-    email_schema = pz.SchemaBuilder.from_file("data/email_schema.yml")
-    assert email_schema is not None
-    assert issubclass(email_schema, pz.TextFile)
+    clinical_schema = pz.SchemaBuilder.from_file(data_path + "synapse_schema.jsonld", schema_type=pz.TextFile)
+    assert clinical_schema is not None
 
 def test_dynamicschema_csv():
-    email_schema = pz.SchemaBuilder.from_file("data/email_schema.csv", schema_type=pz.TextFile)
+    clinical_schema = pz.SchemaBuilder.from_file(data_path + "/synapse_schema.csv", schema_type=pz.TextFile)
+    assert clinical_schema is not None
+
+def test_dynamicschema_json():
+    email_schema = pz.SchemaBuilder.from_file(data_path + "/email_schema.json")
     assert email_schema is not None
     assert issubclass(email_schema, pz.TextFile)
+
+    dataset_id = "enron-eval-tiny"
+    emails = pz.Dataset(dataset_id, schema=email_schema)
+    emails = emails.filter(
+        'The email refers to a fraudulent scheme (i.e., "Raptor", "Deathstar", "Chewco", and/or "Fat Boy")'
+    )
+    emails = emails.filter(
+        "The email is not quoting from a news article or an article written by someone outside of Enron"
+    )
+
+    records, stats = pz.Execute(
+        emails,
+        policy=pz.MinCost(),
+        available_models=[Model.GPT_3_5],
+        num_samples=3,
+        nocache=True,
+        allow_bonded_query=True,
+        allow_code_synth=False,
+        allow_token_reduction=False,
+        execution_engine=pz.SequentialSingleThreadNoSentinelExecution,
+    )
+
+    for rec in records:
+        print(rec.as_dict())
+
+
+def test_dynamicschema_yml():
+    email_schema = pz.SchemaBuilder.from_file(data_path + "/email_schema.yml")
+    assert email_schema is not None
+    assert issubclass(email_schema, pz.TextFile)
+
+    dataset_id = "enron-eval-tiny"
+    emails = pz.Dataset(dataset_id, schema=email_schema)
+    emails = emails.filter(
+        'The email refers to a fraudulent scheme (i.e., "Raptor", "Deathstar", "Chewco", and/or "Fat Boy")'
+    )
+    emails = emails.filter(
+        "The email is not quoting from a news article or an article written by someone outside of Enron"
+    )
+
+    records, stats = pz.Execute(
+        emails,
+        policy=pz.MinCost(),
+        available_models=[Model.GPT_3_5],
+        num_samples=3,
+        nocache=True,
+        allow_bonded_query=True,
+        allow_code_synth=False,
+        allow_token_reduction=False,
+        execution_engine=pz.SequentialSingleThreadNoSentinelExecution,
+    )
+
+    for rec in records:
+        print(rec.as_dict())
