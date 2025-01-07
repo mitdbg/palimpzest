3f1af504a14303bf6bb4664389b2b70990c17084
Michael Cafarella <michael.cafarella@gmail.com>
2025-01-07 14:17:41 -0500

    Remove unneeded timing information

--- src/cli/cli_main.py
+++ src/cli/cli_main.py
@@ -103,13 +103,8 @@ def ls_data() -> None:
     """
     # fetch list of registered datasets
     # Time the loading of the library
-    import time
-    start = time.time()
     import palimpzest as pz
-    end = time.time()
-    print(f"Time taken for PZ import: {end - start} seconds")
     import palimpzest.datamanager.datamanager as pzdm
-
 
     ds = pzdm.DataDirectory().list_registered_datasets()
 
