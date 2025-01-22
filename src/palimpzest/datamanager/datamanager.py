79ae4077d055ef75cbbc285c3ad05731bc7dbcb6
Matthew Russo <mdrusso@mit.edu>
2025-01-22 10:18:05 -0500

    check that fix throws error in CI pipeline

--- src/palimpzest/datamanager/datamanager.py
+++ src/palimpzest/datamanager/datamanager.py
@@ -61,6 +61,11 @@ class DataDirectory(metaclass=DataDirectorySingletonMeta):
         self._tempCache = {}
         self.cacheService = CacheService()
 
+        try:
+            pass
+        except:
+            pass
+
         # set up data directory
         self._dir = PZ_DIR
         current_config_path = os.path.join(self._dir, "current_config.yaml")
