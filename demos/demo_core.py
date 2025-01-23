3a563f95deaed55089b625bbd9f9e3ddcf137922
chjuncn <130543538+chjuncn@users.noreply.github.com>
2025-01-23 16:25:18 +0800

    update demo-core

--- demos/demo_core.py
+++ demos/demo_core.py
@@ -7,9 +7,9 @@ from palimpzest.core.elements.groupbysig import GroupBySig
 from palimpzest.core.elements.records import DataRecord
 from palimpzest.core.lib.fields import Field
 from palimpzest.core.lib.schemas import ImageFile, Number, PDFFile, TextFile
-from palimpzest.query import Execute
 from palimpzest.sets import Dataset
 from tabulate import tabulate
+from palimpzest.query.processor.config import QueryProcessorConfig
 
 
 class ScientificPaper(PDFFile):
@@ -178,19 +178,20 @@ def get_task_config(task, datasetid):
     
     return root_set, cols, stat_path
 
-def execute_task(task, datasetid, execution_engine, policy, verbose=False, profile=False):
+def execute_task(task, datasetid, processing_strategy, execution_strategy, optimizer_strategy, policy, verbose=False, profile=False):
     """Execute a task and return results"""
     root_set, cols, stat_path = get_task_config(task, datasetid)
-
-    records, execution_stats = Execute(
-        root_set,
+    config = QueryProcessorConfig(
         policy=policy,
         nocache=True,
+        verbose=verbose,
         allow_token_reduction=False,
         allow_code_synth=False,
-        execution_engine=execution_engine,
-        verbose=verbose,
+        processing_strategy=processing_strategy,
+        execution_strategy=execution_strategy,
+        optimizer_strategy=optimizer_strategy,
     )
+    records, execution_stats = root_set.run(config)
 
     if profile:
         os.makedirs("profiling-data", exist_ok=True)
