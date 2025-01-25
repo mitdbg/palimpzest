cad782b35db1afbca1498460b55b7cbc0fbd0cd5
Matthew Russo <mdrusso@mit.edu>
2025-01-25 16:56:24 -0500

    fix webpage content; temp deploy on docs branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,10 @@
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here.
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,17 +13,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Palimpzest'
-copyright = '2025, MIT Data Systems Group'
-author = 'MIT Data Systems Group'
-release = '0.4'
+project = "Palimpzest"
+copyright = "2025, MIT Data Systems Group"
+author = "MIT Data Systems Group"
+release = "0.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
@@ -24,5 +37,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,15 +3,20 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Palimpzest documentation
-========================
+Palimpzest
+==========
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+Check out the :doc:`usage` section for further information, including how to
+:ref:`install <installation>` the project.
 
+.. note::
 
+   This project is under active development.
+
+Contents
+--------
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+   :maxdepth: 1
 
+   usage
+   policy
