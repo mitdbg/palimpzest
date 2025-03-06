1c986d907013538eb260aa094c31c164718a624f
Matthew Russo <mdrusso@mit.edu>
2025-03-06 14:04:32 -0500

    inspect frontier updates

--- demos/optimizer-demo.py
+++ demos/optimizer-demo.py
@@ -21,18 +21,6 @@ biodex_entry_cols = [
     {"name": "fulltext", "type": str, "desc": "The full text of the medical paper, which contains information relevant for creating a drug safety report."},
 ]
 
-biodex_serious_cols = [
-    {"name": "serious", "type": int, "desc": "The seriousness of the adverse event.\n - Equal to 1 if the adverse event resulted in death, a life threatening condition, hospitalization, disability, congenital anomaly, or any other serious condition.\n - If none of the above occurred, equal to 2."},
-]
-
-biodex_patient_sex_cols = [
-    {"name": "patientsex", "type": int, "desc": "The reported biological sex of the patient.\n - Equal to 0 for unknown, 1 for male, 2 for female."},
-]
-
-biodex_drugs_cols = [
-    {"name": "drugs", "type": list[str], "desc": "The list of all active substance names of the drugs discussed in the report."},
-]
-
 biodex_reactions_cols = [
     {"name": "reactions", "type": list[str], "desc": "The list of all medical conditions discussed in the report."},
 ]
@@ -188,9 +176,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run a simple demo")
     parser.add_argument("--verbose", default=False, action="store_true", help="Print verbose output")
     parser.add_argument(
-        "--workload", type=str, help="The workload to run. One of enron, real-estate, biodex, biodex-reactions."
-    )
-    parser.add_argument(
         "--processing-strategy",
         default="sentinel",
         type=str,
@@ -250,20 +235,6 @@ if __name__ == "__main__":
         type=int,
         help="Total sample budget in Random Sampling or MAB sentinel execution",
     )
-    parser.add_argument("--sample-all-ops", default=False, action="store_true", help="Sample all operators")
-    parser.add_argument("--sample-all-records", default=False, action="store_true", help="Sample all records")
-    parser.add_argument(
-        "--sample-start-idx",
-        default=None,
-        type=int,
-        help="",
-    )
-    parser.add_argument(
-        "--sample-end-idx",
-        default=None,
-        type=int,
-        help="",
-    )
     parser.add_argument(
         "--exp-name",
         default=None,
@@ -284,17 +255,12 @@ if __name__ == "__main__":
     # create directory for profiling data
     os.makedirs("opt-profiling-data", exist_ok=True)
 
-    workload = args.workload
     verbose = args.verbose
     seed = args.seed
     val_examples = args.val_examples
     k = args.k
     j = args.j
     sample_budget = args.sample_budget
-    sample_all_ops = args.sample_all_ops
-    sample_all_records = args.sample_all_records
-    sample_start_idx = args.sample_start_idx
-    sample_end_idx = args.sample_end_idx
     exp_name = args.exp_name
 
     policy = pz.MaxQuality()
@@ -311,178 +277,106 @@ if __name__ == "__main__":
     if os.getenv("OPENAI_API_KEY") is None and os.getenv("TOGETHER_API_KEY") is None:
         print("WARNING: Both OPENAI_API_KEY and TOGETHER_API_KEY are unset")
 
-    # create pz plan
-    plan, val_datasource, use_final_op_quality = None, None, False
-    if workload == "biodex-reactions":
-        # create data source
-        datareader = BiodexReader(
-            reactions_only=True,
-            split="test",
-            num_samples=250,
-            shuffle=False,
-            seed=seed,
-        )
+    # create data source
+    datareader = BiodexReader(
+        reactions_only=True,
+        split="test",
+        num_samples=250,
+        shuffle=False,
+        seed=seed,
+    )
 
-        # create validation data source
-        val_datasource = BiodexReader(
-            reactions_only=True,
-            split="train",
-            num_samples=val_examples,
-            shuffle=False,
-            seed=seed,
-        )
+    # create validation data source
+    val_datasource = BiodexReader(
+        reactions_only=True,
+        split="train",
+        num_samples=val_examples,
+        shuffle=False,
+        seed=seed,
+    )
 
-        # # load index [Colbert]
-        # index_path = ".ragatouille/colbert/indexes/reaction-terms"
-        # index = RAGPretrainedModel.from_index(index_path)
+    # # load index [Colbert]
+    # index_path = ".ragatouille/colbert/indexes/reaction-terms"
+    # index = RAGPretrainedModel.from_index(index_path)
 
-        # def search_func(index, query, k):
-        #     results = index.search(query, k=1)
-        #     results = [result[0] if isinstance(result, list) else result for result in results]
-        #     sorted_results = sorted(results, key=lambda result: result["score"], reverse=True)
-        #     return [result["content"] for result in sorted_results[:k]], GenerationStats(model_name="colbert")
+    # def search_func(index, query, k):
+    #     results = index.search(query, k=1)
+    #     results = [result[0] if isinstance(result, list) else result for result in results]
+    #     sorted_results = sorted(results, key=lambda result: result["score"], reverse=True)
+    #     return [result["content"] for result in sorted_results[:k]], GenerationStats(model_name="colbert")
 
-        # load index [text-embedding-3-small]
-        chroma_client = chromadb.PersistentClient(".chroma")
-        openai_ef = OpenAIEmbeddingFunction(
-            api_key=os.environ["OPENAI_API_KEY"],
-            model_name="text-embedding-3-small",
-        )
-        index = chroma_client.get_collection("biodex-reaction-terms", embedding_function=openai_ef)
+    # load index [text-embedding-3-small]
+    chroma_client = chromadb.PersistentClient(".chroma")
+    openai_ef = OpenAIEmbeddingFunction(
+        api_key=os.environ["OPENAI_API_KEY"],
+        model_name="text-embedding-3-small",
+    )
+    index = chroma_client.get_collection("biodex-reaction-terms", embedding_function=openai_ef)
 
-        def search_func(index: chromadb.Collection, query: list[list[float]], k: int) -> list[str]:
-            # execute query with embeddings
-            results_per_query = int(50 / len(query))  # NOTE: 50 is chosen to ~match k=49 in Lotus / DocETL evaluation
-            results = index.query(query, n_results=results_per_query)
+    def search_func(index: chromadb.Collection, query: list[list[float]], k: int) -> list[str]:
+        # execute query with embeddings
+        results_per_query = int(50 / len(query))  # NOTE: 50 is chosen to ~match k=49 in Lotus / DocETL evaluation
+        results = index.query(query, n_results=results_per_query)
 
-            # get list of result terms with their cosine similarity scores
-            final_results = []
-            for query_docs, query_distances in zip(results["documents"], results["distances"]):
-                for doc, dist in zip(query_docs, query_distances):
-                    cosine_similarity = 1 - dist
-                    final_results.append({"content": doc, "similarity": cosine_similarity})
+        # get list of result terms with their cosine similarity scores
+        final_results = []
+        for query_docs, query_distances in zip(results["documents"], results["distances"]):
+            for doc, dist in zip(query_docs, query_distances):
+                cosine_similarity = 1 - dist
+                final_results.append({"content": doc, "similarity": cosine_similarity})
 
-            # sort the results by similarity score
-            sorted_results = sorted(final_results, key=lambda result: result["similarity"], reverse=True)
+        # sort the results by similarity score
+        sorted_results = sorted(final_results, key=lambda result: result["similarity"], reverse=True)
 
-            # remove duplicates
-            sorted_results_set = set()
-            final_sorted_results = []
-            for result in sorted_results:
-                if result["content"] not in sorted_results_set:
-                    sorted_results_set.add(result["content"])
-                    final_sorted_results.append(result["content"])
+        # remove duplicates
+        sorted_results_set = set()
+        final_sorted_results = []
+        for result in sorted_results:
+            if result["content"] not in sorted_results_set:
+                sorted_results_set.add(result["content"])
+                final_sorted_results.append(result["content"])
 
-            # return the top-k similar results and generation stats
-            return final_sorted_results[:k]
+        # return the top-k similar results and generation stats
+        return final_sorted_results[:k]
 
-        def store_og_reactions(record: dict) -> dict:
-            """Store the original reactions in a separate column."""
-            return {"og_reaction_labels": record["reaction_labels"]}
+    def store_og_reactions(record: dict) -> dict:
+        """Store the original reactions in a separate column."""
+        return {"og_reaction_labels": record["reaction_labels"]}
 
-        def trim_terms(record: dict) -> dict:
-            """Only keep `reaction_labels` for which every word appears in the record's `fulltext`."""
-            reaction_labels = [label.lower().replace("'", "").replace("^", "") for label in record["reaction_labels"]]
-            fulltext = record["fulltext"].lower().replace("'", "").replace("^", "")
-            trimmed_reaction_labels = [
-                label
-                for label in reaction_labels
-                if all(word in fulltext for word in label.split(" "))
-            ]
-            record["reaction_labels"] = trimmed_reaction_labels
+    def trim_terms(record: dict) -> dict:
+        """Only keep `reaction_labels` for which every word appears in the record's `fulltext`."""
+        reaction_labels = [label.lower().replace("'", "").replace("^", "") for label in record["reaction_labels"]]
+        fulltext = record["fulltext"].lower().replace("'", "").replace("^", "")
+        trimmed_reaction_labels = [
+            label
+            for label in reaction_labels
+            if all(word in fulltext for word in label.split(" "))
+        ]
+        record["reaction_labels"] = trimmed_reaction_labels
 
-            print(f"Trimmed reaction labels: {trimmed_reaction_labels}")
+        print(f"Trimmed reaction labels: {trimmed_reaction_labels}")
 
-            return record
+        return record
 
-        # construct plan
-        plan = pz.Dataset(datareader)
-        plan = plan.sem_add_columns(biodex_reactions_cols)
-        plan = plan.retrieve(
-            index=index,
-            search_func=search_func,
-            search_attr="reactions",
-            output_attr="reaction_labels",
-            output_attr_desc="Most relevant official terms for adverse reactions for the provided `reactions`",
-        )
-        # plan = plan.add_columns(store_og_reactions, cols=[{"name": "og_reaction_labels", "type": list[str], "desc": ""}], depends_on=["reaction_labels"])
-        # plan = plan.map(trim_terms, depends_on=["reaction_labels"])
-        plan = plan.sem_add_columns(biodex_ranked_reactions_labels_cols, depends_on=["title", "abstract", "fulltext", "reaction_labels"])
+    # construct plan
+    plan = pz.Dataset(datareader)
+    plan = plan.sem_add_columns(biodex_reactions_cols)
+    plan = plan.retrieve(
+        index=index,
+        search_func=search_func,
+        search_attr="reactions",
+        output_attr="reaction_labels",
+        output_attr_desc="Most relevant official terms for adverse reactions for the provided `reactions`",
+    )
+    # plan = plan.add_columns(store_og_reactions, cols=[{"name": "og_reaction_labels", "type": list[str], "desc": ""}], depends_on=["reaction_labels"])
+    # plan = plan.map(trim_terms, depends_on=["reaction_labels"])
+    plan = plan.sem_add_columns(biodex_ranked_reactions_labels_cols, depends_on=["title", "abstract", "fulltext", "reaction_labels"])
 
-        # only use final op quality
-        use_final_op_quality = True
+    # only use final op quality
+    use_final_op_quality = True
 
-    elif workload == "biodex":
-        # create data source
-        datareader = BiodexReader(
-            reactions_only=False,
-            split="test",
-            num_samples=250,
-            shuffle=False,
-            seed=seed,
-        )
-
-        # create validation data source
-        val_datasource = BiodexReader(
-            reactions_only=False,
-            split="train",
-            num_samples=val_examples,
-            shuffle=False,
-            seed=seed,
-        )
-
-        # load index
-        index_path = ".ragatouille/colbert/indexes/reaction-terms"
-        index = RAGPretrainedModel.from_index(index_path)
-
-        # construct plan
-        plan = pz.Dataset(datareader)
-        plan = plan.sem_add_columns(biodex_serious_cols, depends_on=["title", "abstract", "fulltext"])
-        plan = plan.sem_add_columns(biodex_patient_sex_cols, depends_on=["title", "abstract", "fulltext"])
-        plan = plan.sem_add_columns(biodex_drugs_cols, depends_on=["title", "abstract", "fulltext"])
-        plan = plan.sem_add_columns(biodex_reactions_cols, depends_on=["title", "abstract", "fulltext"])
-
-        def search_func(index, query, k):
-            results = index.search(query, k=1)
-            results = [result[0] if isinstance(result, list) else result for result in results]
-            sorted_results = sorted(results, key=lambda result: result["score"], reverse=True)
-            return [result["content"] for result in sorted_results[:k]]
-
-        plan = plan.retrieve(
-            index=index,
-            search_func=search_func,
-            search_attr="reactions",
-            output_attr="reaction_labels",
-            output_attr_desc="Most relevant official terms for adverse reactions for the provided `reactions`",
-            # k=10, # if we set k, then it will be fixed; if we leave it unspecified then the optimizer will choose
-        )
-        plan = plan.sem_add_columns(biodex_ranked_reactions_labels_cols)
-
-        # only use final op quality
-        use_final_op_quality = True
-
-    # select optimization strategy and available models based on engine
-    optimizer_strategy, available_models = None, None
-    if args.processing_strategy == "sentinel":
-        optimizer_strategy = "pareto"
-        available_models = get_models(include_vision=True)
-    else:
-        model_str_to_model = {
-            "gpt-4o": Model.GPT_4o,
-            "gpt-4o-mini": Model.GPT_4o_MINI,
-            "mixtral": Model.MIXTRAL,
-            "deepseek": Model.DEEPSEEK,
-            "llama": Model.LLAMA3,
-        }
-        model_str_to_vision_model = {
-            "gpt-4o": Model.GPT_4o_V,
-            "gpt-4o-mini": Model.GPT_4o_MINI_V,
-            "mixtral": Model.LLAMA3_V,
-            "deepseek": Model.LLAMA3_V,
-            "llama": Model.LLAMA3_V,
-        }
-        optimizer_strategy = "none"
-        available_models = [model_str_to_model[args.model]] + [model_str_to_vision_model[args.model]]
+    # fetch available models
+    available_models = get_models(include_vision=True)
 
     # execute pz plan
     config = pz.QueryProcessorConfig(
@@ -490,7 +384,7 @@ if __name__ == "__main__":
         cache=False,
         val_datasource=val_datasource,
         processing_strategy=args.processing_strategy,
-        optimizer_strategy=optimizer_strategy,
+        optimizer_strategy="pareto",
         sentinel_execution_strategy=args.sentinel_execution_strategy,
         execution_strategy=args.execution_strategy,
         use_final_op_quality=use_final_op_quality,
@@ -520,43 +414,34 @@ if __name__ == "__main__":
         k=k,
         j=j,
         sample_budget=sample_budget,
-        sample_all_ops=sample_all_ops,
-        sample_all_records=sample_all_records,
-        sample_start_idx=sample_start_idx,
-        sample_end_idx=sample_end_idx,
         seed=seed,
         exp_name=exp_name,
     )
 
     print(data_record_collection.to_df())
-    data_record_collection.to_df().to_csv(f"opt-profiling-data/{workload}-{exp_name}-output.csv", index=False)
+    data_record_collection.to_df().to_csv(f"opt-profiling-data/biodex-reactions-{exp_name}-output.csv", index=False)
 
     # create filepaths for records and stats
     records_path = (
-        f"opt-profiling-data/{workload}-{exp_name}-records.json"
+        f"opt-profiling-data/biodex-reactions-{exp_name}-records.json"
         if args.processing_strategy in ["mab_sentinel", "random_sampling"]
-        else f"opt-profiling-data/{workload}-baseline-{exp_name}-records.json"
+        else f"opt-profiling-data/biodex-reactions-{exp_name}-records.json"
     )
     stats_path = (
-        f"opt-profiling-data/{workload}-{exp_name}-profiling.json"
+        f"opt-profiling-data/biodex-reactions-{exp_name}-profiling.json"
         if args.processing_strategy in ["mab_sentinel", "random_sampling"]
-        else f"opt-profiling-data/{workload}-baseline-{exp_name}-profiling.json"
+        else f"opt-profiling-data/biodex-reactions-{exp_name}-profiling.json"
     )
 
     # save record outputs
     record_jsons = []
     for record in data_record_collection:
         record_dict = record.to_dict()
-        if workload == "biodex":
-            record_dict = {
-                k: v for k, v in record_dict.items() if k in ["pmid", "serious", "patientsex", "drugs", "reactions"]
-            }
-        elif workload == "biodex-reactions":
-            record_dict = {
-                k: v
-                for k, v in record_dict.items()
-                if k in ["pmid", "reactions", "og_reaction_labels", "reaction_labels", "ranked_reaction_labels"]
-            }
+        record_dict = {
+            k: v
+            for k, v in record_dict.items()
+            if k in ["pmid", "reactions", "og_reaction_labels", "reaction_labels", "ranked_reaction_labels"]
+        }
         record_jsons.append(record_dict)
 
     with open(records_path, "w") as f:
