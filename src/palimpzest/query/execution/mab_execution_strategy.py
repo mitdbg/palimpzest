043bdf5755999710355ac3a21c2514c3adeae9ac
Matthew Russo <mdrusso@mit.edu>
2025-03-06 21:59:18 -0500

    debug

--- src/palimpzest/query/execution/mab_execution_strategy.py
+++ src/palimpzest/query/execution/mab_execution_strategy.py
@@ -139,10 +139,13 @@ class OpFrontier:
         }
 
         # compute avg. selectivity, cost, time, and quality for each physical operator
-        phys_op_to_mean_selectivity = {
-            op_id: len(record_op_stats_lst) / sum([record_op_stats.passed_operator for record_op_stats in record_op_stats_lst])
-            for op_id, record_op_stats_lst in phys_op_id_to_record_op_stats.items()
-        }
+        try:
+            phys_op_to_mean_selectivity = {
+                op_id: len(record_op_stats_lst) / sum([record_op_stats.passed_operator for record_op_stats in record_op_stats_lst])
+                for op_id, record_op_stats_lst in phys_op_id_to_record_op_stats.items()
+            }
+        except:
+            import pdb; pdb.set_trace()
         phys_op_to_mean_cost = {
             op_id: np.mean([record_op_stats.cost_per_record for record_op_stats in record_op_stats_lst])
             for op_id, record_op_stats_lst in phys_op_id_to_record_op_stats.items()
