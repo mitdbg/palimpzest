68ee742b2cdb3a30771a25df7be5c41574400364
Matthew Russo <mdrusso@mit.edu>
2025-03-10 20:35:00 -0400

    up max workers

--- demos/optimizer-demo.py
+++ demos/optimizer-demo.py
@@ -362,7 +362,7 @@ if __name__ == "__main__":
         sentinel_execution_strategy=sentinel_execution_strategy,
         execution_strategy=execution_strategy,
         use_final_op_quality=use_final_op_quality,
-        max_workers=10,
+        max_workers=20,
         verbose=verbose,
         available_models=[
             # Model.GPT_4o,
