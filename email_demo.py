3e087af825b129f598c3874347216363e34e09c3
Matthew Russo <mdrusso@mit.edu>
2025-07-24 15:01:42 -0400

    adding tools

--- email_demo.py
+++ email_demo.py
@@ -1,10 +1,14 @@
 import argparse
-import palimpzest as pz
-
 import json
 import os
+
+import litellm
 import pandas as pd
-from smolagents import tool, CodeAgent, LiteLLMModel
+from smolagents import CodeAgent, LiteLLMModel, tool
+
+import palimpzest as pz
+from palimpzest.query.generators.generators import Generator
+
 
 # Define tools for the agent to use
 @tool
@@ -13,10 +17,10 @@ def list_filepaths() -> list[str]:
     This tool lists all of the file paths for relevant files in the data directory.
 
     Args:
-    None
+        None
 
     Returns:
-    list[str]: A list of file paths for all files in the dataset directory.
+        list[str]: A list of file paths for all files in the dataset directory.
     """
     dataset_directory = "testdata/enron-eval-medium"
 
@@ -35,19 +39,75 @@ def read_file(filepath: str) -> str:
     It handles both CSV files and html / regular text files.
 
     Args:
-    filepath: The path to the file to read.
+        filepath: The path to the file to read.
 
     Returns:
-    str: The content of the file as a string.
+        str: The content of the file as a string.
     """
     if filepath.endswith(".csv"):
         return pd.read_csv(filepath, encoding="ISO-8859-1").to_string(index=False)
 
-    with open(filepath, 'r', encoding='utf-8') as file:
+    with open(filepath, encoding='utf-8') as file:
         content = file.read()
 
     return content
 
+
+@tool
+def sem_filter(filepath: str, predicate: str) -> bool:
+    """
+    This tool takes a file path and a natural language predicate as input and returns True
+    if the file satisfies the natural language predicate and False otherwise.
+
+    For example, the tool could be invoked as follows to see if a paper is about batteries:
+    ```
+    out = sem_filter("path/to/paper.txt", "This paper is about batteries")
+    ```
+
+    Args:
+        filepath: The path to the file to read.
+        predicate: A natural language predicate (i.e. a filter condition)
+
+    Returns:
+        bool: True if the file satisfies the predicate and False otherwise
+    """
+    with open(filepath) as f:
+        contents = f.read()
+
+    # call prompt factory to get messages
+    messages = [] # TODO
+    generator = Generator()
+    out = litellm.completion(model="anthropic/claude-3-5-sonnet", messages=messages)
+
+
+
+@tool
+def sem_map(filepath: str, fields: dict[str, str]) -> dict:
+    """
+    This tool takes a file path and a dictionary of fields to compute as input and returns
+    the dictionary mapping each field to its computed value. The input dictionary should
+    contain a mapping from the field name to a description of the field.
+
+    For example, the tool could be invoked as follows to extract the title and abstract of a paper:
+    ```
+    fields = {
+        "title": "the title of the paper",
+        "abstract": "the paper's abstract",
+    }
+    out = sem_map("path/to/paper.txt", fields)
+    print(f"Title: {out['title']}")
+    print(f"Abstract: {out['abstract']}")
+    ```
+
+    Args:
+        filepath: The path to the file to read.
+        fields: Dictionary of fields to compute. Keys are field names and values are field descriptions.
+
+    Returns:
+        dict: Dictionary mapping each field to its computed value
+    """
+    # TODO
+    pass
 
 
 def run_agents(model_id="anthropic/claude-3-7-sonnet-latest"):
@@ -83,7 +143,7 @@ def run_pz():
     ds = ds.compute(
         "The sender and subject of each email"
     )
-    return plan.run()
+    return ds.run()
 
 
 if __name__ == "__main__":
