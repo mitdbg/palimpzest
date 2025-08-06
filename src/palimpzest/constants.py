df1e85c822e787073bbc68e4b53199bddbc925f0
Matthew Russo <mdrusso@mit.edu>
2025-08-06 14:38:13 -0400

    add support for o4-mini, gemini-2.5-pro, gemini-2.0-flash, llama-4-maverick

--- src/palimpzest/constants.py
+++ src/palimpzest/constants.py
@@ -19,18 +19,19 @@ class Model(str, Enum):
     DEEPSEEK_R1_DISTILL_QWEN_1_5B = "together_ai/deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
     GPT_4o = "openai/gpt-4o-2024-08-06"
     GPT_4o_MINI = "openai/gpt-4o-mini-2024-07-18"
+    o4_MINI = "openai/o4-mini-2025-04-16"  # noqa: N815
     TEXT_EMBEDDING_3_SMALL = "text-embedding-3-small"
     CLIP_VIT_B_32 = "clip-ViT-B-32"
     CLAUDE_3_5_SONNET = "anthropic/claude-3-5-sonnet-20241022"
     CLAUDE_3_7_SONNET = "anthropic/claude-3-7-sonnet-20250219"
     CLAUDE_3_5_HAIKU = "anthropic/claude-3-5-haiku-20241022"
+    GEMINI_2_0_FLASH = "vertex_ai/gemini-2.0-flash"
+    GEMINI_2_5_PRO = "vertex_ai/gemini-2.5-pro"
+    LLAMA_4_MAVERICK = "vertex_ai/meta/llama-4-maverick-17b-128e-instruct-maas"
     # o1 = "o1-2024-12-17"
 
     def __repr__(self):
         return f"{self.name}"
-
-    def is_deepseek_model(self):
-        return "deepseek" in self.value.lower()
 
     def is_llama_model(self):
         return "llama" in self.value.lower()
@@ -44,12 +45,6 @@ class Model(str, Enum):
     def is_together_model(self):
         return "together_ai" in self.value.lower() or self.is_clip_model()
 
-    def is_gpt_4o_model(self):
-        return "gpt-4o" in self.value.lower()
-
-    def is_o1_model(self):
-        return "o1" in self.value.lower()
-
     def is_text_embedding_model(self):
         return "text-embedding" in self.value.lower()
 
@@ -59,15 +54,26 @@ class Model(str, Enum):
     def is_anthropic_model(self):
         return "anthropic" in self.value.lower()
 
+    def is_vertex_model(self):
+        return "vertex_ai" in self.value.lower()
+
     def is_text_model(self):
         non_text_models = [Model.LLAMA3_2_90B_V, Model.CLIP_VIT_B_32, Model.TEXT_EMBEDDING_3_SMALL]
         return self not in non_text_models
 
     def is_vision_model(self):
-        return self in [Model.LLAMA3_2_90B_V, Model.GPT_4o, Model.GPT_4o_MINI]
+        return self in [
+            Model.LLAMA3_2_90B_V, Model.LLAMA_4_MAVERICK,
+            Model.GPT_4o, Model.GPT_4o_MINI, Model.o4_MINI,
+            Model.GEMINI_2_0_FLASH, Model.GEMINI_2_5_PRO,
+        ]
 
     def is_multimodal_model(self):
-        return self in [Model.GPT_4o, Model.GPT_4o_MINI]
+        return self in [
+            Model.LLAMA_4_MAVERICK,
+            Model.GPT_4o, Model.GPT_4o_MINI, Model.o4_MINI,
+            Model.GEMINI_2_0_FLASH, Model.GEMINI_2_5_PRO,
+        ]
 
     def is_embedding_model(self):
         return self in [Model.CLIP_VIT_B_32, Model.TEXT_EMBEDDING_3_SMALL]
@@ -254,8 +260,6 @@ LLAMA3_2_3B_INSTRUCT_MODEL_CARD = {
     "seconds_per_output_token": 0.0064,
     ##### Agg. Benchmark #####
     "overall": 36.50, # https://huggingface.co/meta-llama/Llama-3.2-3B-Instruct/discussions/13
-    ##### Code #####
-    "code": 0.0,
 }
 LLAMA3_1_8B_INSTRUCT_MODEL_CARD = {
     ##### Cost in USD #####
@@ -265,8 +269,6 @@ LLAMA3_1_8B_INSTRUCT_MODEL_CARD = {
     "seconds_per_output_token": 0.0059,
     ##### Agg. Benchmark #####
     "overall": 44.25,
-    ##### Code #####
-    "code": 72.6,
 }
 LLAMA3_3_70B_INSTRUCT_MODEL_CARD = {
     ##### Cost in USD #####
@@ -276,8 +278,6 @@ LLAMA3_3_70B_INSTRUCT_MODEL_CARD = {
     "seconds_per_output_token": 0.0139,
     ##### Agg. Benchmark #####
     "overall": 65.92,
-    ##### Code #####
-    "code": 88.4,
 }
 LLAMA3_2_90B_V_MODEL_CARD = {
     ##### Cost in USD #####
@@ -296,8 +296,6 @@ MIXTRAL_8X_7B_MODEL_CARD = {
     "seconds_per_output_token": 0.0112,
     ##### Agg. Benchmark #####
     "overall": 43.27,
-    ##### Code #####
-    "code": 40.0,
 }
 DEEPSEEK_V3_MODEL_CARD = {
     ##### Cost in USD #####
@@ -307,8 +305,6 @@ DEEPSEEK_V3_MODEL_CARD = {
     "seconds_per_output_token": 0.0769,
     ##### Agg. Benchmark #####
     "overall": 75.87,
-    ##### Code #####
-    "code": 92.0,
 }
 DEEPSEEK_R1_DISTILL_QWEN_1_5B_MODEL_CARD = {
     ##### Cost in USD #####
@@ -318,8 +314,6 @@ DEEPSEEK_R1_DISTILL_QWEN_1_5B_MODEL_CARD = {
     "seconds_per_output_token": 0.0026,
     ##### Agg. Benchmark #####
     "overall": 39.90, # https://www.reddit.com/r/LocalLLaMA/comments/1iserf9/deepseek_r1_distilled_models_mmlu_pro_benchmarks/
-    ##### Code #####
-    "code": 0.0,
 }
 GPT_4o_MODEL_CARD = {
     # NOTE: it is unclear if the same ($ / token) costs can be applied for vision, or if we have to calculate this ourselves
@@ -330,8 +324,6 @@ GPT_4o_MODEL_CARD = {
     "seconds_per_output_token": 0.0079,
     ##### Agg. Benchmark #####
     "overall": 74.68,
-    ##### Code #####
-    "code": 90.0,
 }
 GPT_4o_MINI_MODEL_CARD = {
     # NOTE: it is unclear if the same ($ / token) costs can be applied for vision, or if we have to calculate this ourselves
@@ -342,8 +334,16 @@ GPT_4o_MINI_MODEL_CARD = {
     "seconds_per_output_token": 0.0098,
     ##### Agg. Benchmark #####
     "overall": 63.09,
-    ##### Code #####
-    "code": 86.0,
+}
+o4_MINI_MODEL_CARD = {  # noqa: N816
+    # NOTE: it is unclear if the same ($ / token) costs can be applied for vision, or if we have to calculate this ourselves
+    ##### Cost in USD #####
+    "usd_per_input_token": 1.1 / 1e6,
+    "usd_per_output_token": 4.4 / 1e6,
+    ##### Time #####
+    "seconds_per_output_token": 0.0093,
+    ##### Agg. Benchmark #####
+    "overall": 79.40,  # using number reported for o3-mini; true number is likely higher
 }
 o1_MODEL_CARD = {  # noqa: N816
     # NOTE: it is unclear if the same ($ / token) costs can be applied for vision, or if we have to calculate this ourselves
@@ -354,8 +354,6 @@ o1_MODEL_CARD = {  # noqa: N816
     "seconds_per_output_token": 0.0110,
     ##### Agg. Benchmark #####
     "overall": 89.30,
-    ##### Code #####
-    "code": 92.3, # NOTE: just copying MMLU score for now
 }
 TEXT_EMBEDDING_3_SMALL_MODEL_CARD = {
     ##### Cost in USD #####
@@ -402,6 +400,33 @@ CLAUDE_3_5_HAIKU_MODEL_CARD = {
     ##### Agg. Benchmark #####
     "overall": 70.00,  # hard-coding to be slightly worse than Claude 3.5 Sonnet
 }
+GEMINI_2_0_FLASH_MODEL_CARD = {
+    ##### Cost in USD #####
+    "usd_per_input_token": 0.15 / 1e6,
+    "usd_per_output_token": 0.6 / 1e6,
+    ##### Time #####
+    "seconds_per_output_token": 0.0049,
+    ##### Agg. Benchmark #####
+    "overall": 77.60,
+}
+GEMINI_2_5_PRO_MODEL_CARD = {
+    ##### Cost in USD #####
+    "usd_per_input_token": 1.25 / 1e6,
+    "usd_per_output_token": 10.0 / 1e6,
+    ##### Time #####
+    "seconds_per_output_token": 0.0070,
+    ##### Agg. Benchmark #####
+    "overall": 86.00,
+}
+LLAMA_4_MAVERICK_MODEL_CARD = {
+    ##### Cost in USD #####
+    "usd_per_input_token": 0.35 / 1e6,
+    "usd_per_output_token": 1.15 / 1e6,
+    ##### Time #####
+    "seconds_per_output_token": 0.0058,
+    ##### Agg. Benchmark #####
+    "overall": 80.50,
+}
 
 MODEL_CARDS = {
     Model.LLAMA3_2_3B.value: LLAMA3_2_3B_INSTRUCT_MODEL_CARD,
@@ -413,124 +438,14 @@ MODEL_CARDS = {
     Model.MIXTRAL.value: MIXTRAL_8X_7B_MODEL_CARD,
     Model.GPT_4o.value: GPT_4o_MODEL_CARD,
     Model.GPT_4o_MINI.value: GPT_4o_MINI_MODEL_CARD,
+    Model.o4_MINI.value: o4_MINI_MODEL_CARD,
     # Model.o1.value: o1_MODEL_CARD,
     Model.TEXT_EMBEDDING_3_SMALL.value: TEXT_EMBEDDING_3_SMALL_MODEL_CARD,
     Model.CLIP_VIT_B_32.value: CLIP_VIT_B_32_MODEL_CARD,
     Model.CLAUDE_3_5_SONNET.value: CLAUDE_3_5_SONNET_MODEL_CARD,
     Model.CLAUDE_3_7_SONNET.value: CLAUDE_3_7_SONNET_MODEL_CARD,
     Model.CLAUDE_3_5_HAIKU.value: CLAUDE_3_5_HAIKU_MODEL_CARD,
+    Model.GEMINI_2_0_FLASH.value: GEMINI_2_0_FLASH_MODEL_CARD,
+    Model.GEMINI_2_5_PRO.value: GEMINI_2_5_PRO_MODEL_CARD,
+    Model.LLAMA_4_MAVERICK.value: LLAMA_4_MAVERICK_MODEL_CARD,
 }
-
-
-###### DEPRECATED ######
-# # NOTE: seconds_per_output_token is based on `gpt-3.5-turbo-1106`
-# GPT_3_5_MODEL_CARD = {
-#     ##### Cost in USD #####
-#     "usd_per_input_token": 0.5 / 1E6,
-#     "usd_per_output_token": 1.5 / 1E6,
-#     ##### Time #####
-#     "seconds_per_output_token": 0.0065,
-#     ##### Agg. Benchmark #####
-#     "overall": 70.0, # 5-shot
-#     ##### Commonsense Reasoning #####
-#     "reasoning": 84.1,
-#     ### "HellaSwag": 85.5,^  # 10-shot
-#     ### "WinoGrande": 81.6,^ # 5-shot
-#     ### "Arc-e": 85.2,^      # 25-shot
-#     ##### World Knowledge #####
-#     ##### Reading Comprehension #####
-#     ### "DROP": 64.1, # 3-shot
-#     ##### Code #####
-#     "code": 48.1,
-#     ### "HumanEval": 48.1,^ # 0-shot
-#     ##### Math #####
-#     "math": 57.1,
-#     ### "GSM8K": 57.1,^     # 5-shot
-# }
-# # NOTE: the seconds_per_output_token was computed based on a slightly different model ('gpt-4-1106-preview')
-# #       and the benchmark statistics were computed based on the GPT-4 Technical Report; these might be
-# #       slightly innacurate compared to the real numbers for gpt-4-0125-preview, but we'll use them until
-# #       we have something better. (The cost metrics are accurate).
-# GPT_4_MODEL_CARD = {
-#     ##### Cost in USD #####
-#     "usd_per_input_token": 10 / 1E6,
-#     "usd_per_output_token": 30 / 1E6,
-#     ##### Time #####
-#     "seconds_per_output_token": 0.018,
-#     ##### Agg. Benchmark #####
-#     "overall": 86.4, # 5-shot
-#     ##### Commonsense Reasoning #####
-#     "reasoning": 93.0,
-#     ### "HellaSwag": 95.3,^  # 10-shot
-#     ### "WinoGrande": 87.5,^ # 5-shot
-#     ### "Arc-e": 96.3,^      # 25-shot
-#     ##### World Knowledge #####
-#     ##### Reading Comprehension #####
-#     ### "DROP": 80.9, # 3-shot
-#     ##### Code #####
-#     "code": 67.0,
-#     ### "HumanEval": 67.0,^ # 0-shot
-#     ##### Math #####
-#     "math": 92.0,
-#     ### "GSM8K": 92.0,^     # 5-shot
-# }
-
-# # TODO: use cost info in here: https://platform.openai.com/docs/guides/vision/calculating-costs
-# GPT_4V_MODEL_CARD = {
-#     ##### Cost in USD #####
-#     "usd_per_input_token": 10 / 1E6,
-#     "usd_per_output_token": 30 / 1E6,
-#     ##### Time #####
-#     "seconds_per_output_token": 0.042 / 10.0, # TODO: / 10.0 is a hack; need to figure out why time estimates are so off
-#     ##### Agg. Benchmark #####
-#     "overall": 86.4,
-# }
-
-
-# GEMINI_1_MODEL_CARD = {
-#     ##### Cost in USD #####
-#     "usd_per_input_token": 125 / 1E8, # Gemini is free but rate limited for now. Pricing will be updated
-#     "usd_per_output_token": 375 / 1E9,
-#     ##### Time #####
-#     "seconds_per_output_token": 0.042 / 10.0, # TODO:
-#     ##### Agg. Benchmark #####
-#     "overall": 65.0, # 90.0 TODO: we are using the free version of Gemini which is substantially worse than its paid version; I'm manually revising it's quality below that of Mixtral
-#     ##### Commonsense Reasoning #####
-#     "reasoning": 80.0, # 87.8, TODO: see note above on overall
-#     # "HellaSwag": 87.8,  # 10-shot
-#     ##### World Knowledge #####
-#     ##### Reading Comprehension #####
-#     # "DROP": 82.4, # Variable shots ?
-#     ##### Code #####
-#     "code": 74.4,
-#     # "HumanEval": 74.4, # 0-shot (IT)*
-#     # "Natural2Code": 74.9, # 0-shot
-#     ##### Math #####
-#     "math": 94.4,
-#     # "GSM8K": 94.4,     # maj1@32
-#     # "MATH": 53.2,      # 4-shot
-# }
-
-# GEMINI_1V_MODEL_CARD = {
-#     ##### Cost in USD #####
-#     "usd_per_input_token": 25 / 1E6,  # Gemini is free but rate limited for now. Pricing will be updated
-#     "usd_per_output_token": 375 / 1E9,
-#     ##### Time #####
-#     "seconds_per_output_token": 0.042, # / 10.0, # TODO:
-#     ##### Agg. Benchmark #####
-#     "overall": 65.0, # 90.0, TODO: see note above in Gemini_1 model card
-#     ##### Commonsense Reasoning #####
-#     "reasoning": 80.0, # 87.8, TODO: see note above in Gemini_1 model card
-#     # "HellaSwag": 87.8,  # 10-shot
-#     ##### World Knowledge #####
-#     ##### Reading Comprehension #####
-#     # "DROP": 82.4, # Variable shots ?
-#     ##### Code #####
-#     "code": 74.4,
-#     # "HumanEval": 74.4, # 0-shot (IT)*
-#     # "Natural2Code": 74.9, # 0-shot
-#     ##### Math #####
-#     "math": 94.4,
-#     # "GSM8K": 94.4,     # maj1@32
-#     # "MATH": 53.2,      # 4-shot
-# }
