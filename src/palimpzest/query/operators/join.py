4c7bc4bb49b3a89da434f5fc1b39f79dfc638042
Bari Bo LeBari <143016395+lilbarbar@users.noreply.github.com>
2025-11-04 22:31:10 -0500

    Blebari task 131 (#241)
    * .
    
    * .
    
    * minor tweaks
    
    * add embedding costs to RecordOpStats
    
    * minor tweaks
    
    * change comment
    
    ---------
    
    Co-authored-by: Bari LeBari <barilebari@dhcp-10-29-128-127.dyn.mit.edu>
    Co-authored-by: Bari LeBari <barilebari@Baris-MacBook-Pro.local>
    Co-authored-by: Matthew Russo <mdrusso@mit.edu>

--- src/palimpzest/query/operators/join.py
+++ src/palimpzest/query/operators/join.py
@@ -376,8 +376,10 @@ class LLMJoin(JoinOp):
             join_condition=self.condition,
             total_input_tokens=generation_stats.total_input_tokens,
             total_output_tokens=generation_stats.total_output_tokens,
+            total_embedding_input_tokens=generation_stats.total_embedding_input_tokens,
             total_input_cost=generation_stats.total_input_cost,
             total_output_cost=generation_stats.total_output_cost,
+            total_embedding_cost=generation_stats.total_embedding_cost,
             llm_call_duration_secs=generation_stats.llm_call_duration_secs,
             fn_call_duration_secs=generation_stats.fn_call_duration_secs,
             total_llm_calls=generation_stats.total_llm_calls,
@@ -584,13 +586,13 @@ class EmbeddingJoin(LLMJoin):
             return np.zeros((0, 512)), GenerationStats()
 
         start_time = time.time()
-        total_input_tokens = 0
+        total_embedding_input_tokens = 0
         embeddings = None
         if self.text_only:
             client = OpenAI()
             inputs = [dr.to_json_str(bytes_to_str=True, project_cols=input_fields, sorted=True) for dr in candidates]
             response = client.embeddings.create(input=inputs, model=self.embedding_model.value)
-            total_input_tokens = response.usage.total_tokens
+            total_embedding_input_tokens = response.usage.total_tokens
             embeddings = np.array([item.embedding for item in response.data])
         else:
             model = self.locks.get_model(self.embedding_model.value)
@@ -616,14 +618,16 @@ class EmbeddingJoin(LLMJoin):
 
         # compute cost of embedding(s)
         model_card = MODEL_CARDS[self.embedding_model.value]
-        total_input_cost = model_card["usd_per_input_token"] * total_input_tokens
+        total_embedding_cost = model_card["usd_per_input_token"] * total_embedding_input_tokens
         embedding_gen_stats = GenerationStats(
             model_name=self.embedding_model.value,
-            total_input_tokens=total_input_tokens,
+            total_input_tokens=0.0,
             total_output_tokens=0.0,
-            total_input_cost=total_input_cost,
+            total_embedding_input_tokens=total_embedding_input_tokens,
+            total_input_cost=0.0,
             total_output_cost=0.0,
-            cost_per_record=total_input_cost,
+            total_embedding_cost=total_embedding_cost,
+            cost_per_record=total_embedding_cost,
             llm_call_duration_secs=time.time() - start_time,
             total_llm_calls=1,
             total_embedding_llm_calls=len(candidates),
