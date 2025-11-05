5da144b40269b98cece069ce42b59086bfe8d892
Matthew Russo <mdrusso@mit.edu>
2025-11-04 22:30:10 -0500

    change comment

--- src/palimpzest/query/operators/rag.py
+++ src/palimpzest/query/operators/rag.py
@@ -322,7 +322,7 @@ class RAGFilter(LLMFilter):
         total_embedding_input_tokens = response.usage.total_tokens
         total_embedding_cost = model_card["usd_per_input_token"] * total_embedding_input_tokens
         embed_stats = GenerationStats(
-            model_name=model_name,  # NOTE: this should be overwritten by generation model in convert()
+            model_name=model_name,  # NOTE: this should be overwritten by generation model in filter()
             total_input_tokens=0.0,
             total_output_tokens=0.0,
             total_embedding_input_tokens=total_embedding_input_tokens,
